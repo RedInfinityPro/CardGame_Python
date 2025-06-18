60f6f18850459e20c15cffa0f8c662a60cfa68c0
RedInfinity_Pro <danieltower101501@gmail.com>
2025-06-18 10:30:03 -0400

    Adding menus.

---
+++ Container/imports_library.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+import sys
+import time
+import os
+import pygame_menu
+from pygame_menu import themes
+current_time = time.time()
+random.seed(current_time)
---
+++ main.py
@@ -0,0 +1,63 @@
+from Container.imports_library import *
+from MainMenu.menuFile import *
+
+def on_resize(screen, main_menu, pause_menu) -> None:
+    window_size = screen.get_size()
+    new_w, new_h = window_size[0], window_size[1]
+    main_menu.main_menu.resize(new_w, new_h)
+    main_menu.load_screen.resize(new_w, new_h)
+    main_menu.settings_screen.resize(new_w, new_h)
+    pause_menu.pause_menu.resize(new_w, new_h)
+    pause_menu.settings_screen.resize(new_w, new_h)
+    
+class Application:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("CardGame")
+        self.screenWidth, self.screenHeight = 1280, 720
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight), pygame.RESIZABLE)
+        self.background_surface = pygame.Surface((self.screenWidth, self.screenHeight)).convert()
+        # menu
+        self.main_menu = MainMenu(screen=self.screen, width=self.screenWidth, height=self.screenHeight)
+        self.main_menu.get_main_menu()
+        self.pause_menu =PauseMenu(screen=self.screen, width=self.screenWidth, height=self.screenHeight)
+        self.pause_menu.get_pause_menu()
+        
+    def run(self):
+        while self.running:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    sys.exit()
+                elif event.type == pygame.VIDEORESIZE:
+                    self.screenWidth, self.screenHeight = event.w, event.h
+                    self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight), pygame.RESIZABLE)
+                    self.background_surface = pygame.transform.scale(self.background_surface, (self.screenWidth, self.screenHeight))
+                    on_resize(screen=self.screen, main_menu=self.main_menu, pause_menu=self.pause_menu)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        if self.main_menu.play:
+                            self.pause_menu.resume_game = not(self.pause_menu.resume_game)
+            # menu
+            if not self.main_menu.play or self.pause_menu.exit_to_main:
+                self.main_menu.play = False
+                self.pause_menu.reset_flags()
+                self.main_menu.main_menu.update(events)
+                self.main_menu.main_menu.draw(self.screen)
+            elif self.main_menu.play and not self.pause_menu.resume_game:
+                self.pause_menu.pause_menu.update(events)
+                self.pause_menu.pause_menu.draw(self.screen)
+            else:
+                self.screen.blit(self.background_surface, (0, 0))
+            self.clock.tick(64)
+            pygame.display.flip()
+            pygame.display.update()
+        pygame.quit()
+        sys.exit()
+            
+if __name__ == "__main__":
+    app = Application()
+    app.run()
